a104e8ef4ea00b75a1f16ea4b9f7c1afb4b82a48
denizyesilirmak <denizyesilirmak@gmail.com>
2018-01-23 08:12:46 +0300

    Preloader for http requests

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -8,28 +8,33 @@ import { HttpClient } from "@angular/common/http";
 })
 export class SidebarComponent implements OnInit {
   entrylist:any;
+  showpreloader: boolean = true;
 
 
   constructor(private _http:HttpClient) { }
 
   getToday(){
+    this.showpreloader = true;
     this.entrylist = [];
 
     this._http.get("http://80.209.224.120:5000/today").subscribe(data => {
       this.entrylist = data["Topics"];
+      this.showpreloader = false;
     },
     err => {
       alert("Api Unavaible. /today")
     })
-
+    
   }
 test
   // LİNKLERDEKİ BOŞLUKLAR TİREYE ÇEVRİLECEK
 
   getPopular(){
+    this.showpreloader = true;
     this.entrylist = [];
     this._http.get("http://80.209.224.120:5000/popular").subscribe(data => {
       this.entrylist = data["Topics"];
+      this.showpreloader = false;
 
     },
     err => {
