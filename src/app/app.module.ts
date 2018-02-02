c8921faa1ae63a1f66700f5300a1a3ca62219aa6
denizyesilirmak <denizyesilirmak@gmail.com>
2018-02-02 11:36:07 +0300

    Initial Commit for new architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from "@angular/common/http";
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { EntriesComponent } from './entries/entries.component';
-
-const appRoutes: Routes = [
-  { path: 'entry/:title', component: EntriesComponent },
-];
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    SidebarComponent,
-    EntriesComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-    RouterModule.forRoot(appRoutes)
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
