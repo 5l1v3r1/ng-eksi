e015f9c7fae15938043a60c3ad71026c63981b47
Yigitcan Alanyali <y.alanyali@gmail.com>
2018-09-19 17:49:41 +0200

    Update custom.pipe.ts
    Fixed links.

--- src/app/custom.pipe.ts
+++ src/app/custom.pipe.ts
@@ -5,26 +5,27 @@ export class BakinizPipe implements PipeTransform {
   
   transform(value: string, params: any) {
     // Normal bkz
-    value = value.replace(/\(bkz: ?([^)]+)\)/g, (match, p1: any) => {
+    value = value.replace(/\(bkz: ?([^)]+)\)/g, (match, p1: string) => {
       return `(bkz: <a href="/entry/${p1}/1">${p1}</a>)`;
     });
 
     // Gizli bkz
-    value = value.replace(/`([^`:]+)`/g, (match, p1: any) => {
+    value = value.replace(/`([^`:]+)`/g, (match, p1: string) => {
       return `<a href="/entry/${p1}/1">${p1}</a>`;
     });
 
     // Yildizli bkz
-    value = value.replace(/`:([^`]+)`/g, (match, p1: any) => {
+    value = value.replace(/`:([^`]+)`/g, (match, p1: string) => {
       return `<a href="/entry/${p1}/1">*</a>`;
     });
 
-    // Links   
-    value = value.replace(/\[([^ ]+) (.*)\]/g, (match, p1: any) => {
-      return `<a href="${p1}">${p1}</a>`;
+    // Gizli link
+    value = value.replace(/\[([^ ]+) (.*)\]/g, (match, p1: string, p2: string) => {
+      return `<a href="${p1}">${p2}</a>`;
     });
 
-    value = value.replace(/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})/g, (match, p1: any) => {
+    // Normal link
+    value = value.replace(/(?: |\n)(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})/g, (match, p1: any) => {
       return `<a href="${p1}">${p1}</a>`;
     });
 
